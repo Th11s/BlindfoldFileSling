ee2dd144ab63ef6830e909568da2c40430b4d685
Ottenhus, Thomas <TG85+Github@outlook.de>
2025-11-01 21:41:51 +0100

    Service Stubs

---
+++ src/FileSling/Services/FileSystemStorageService.cs
@@ -0,0 +1,63 @@
+﻿using Th11s.FileSling.Commands;
+using Th11s.FileSling.Model;
+using Th11s.FileSling.Queries;
+
+namespace Th11s.FileSling.Services;
+
+public class FileSystemStorageService : IFileStorage
+{
+    public Task AppendFile(AppendFile command)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<DirectoryMetadata> CreateDirectory(CreateDirectory command)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<FileMetadata> CreateFile(CreateFile command)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteDirectory(DeleteDirectory command)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteFile(DeleteFile command)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DiscardFile(DiscardFile command)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task FinalizeFile(FinalizeFile command)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<DirectoryMetadata[]> GetDirectories(GetDirectory query)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Stream> GetFile(GetFile query)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<FileMetadata[]> ListDirectory(ListDirectory query)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RenameDirectory(RenameDirectory command)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/FileSling/Services/IFileStorage.cs
@@ -0,0 +1,26 @@
+﻿using Th11s.FileSling.Commands;
+using Th11s.FileSling.Model;
+using Th11s.FileSling.Queries;
+
+namespace Th11s.FileSling.Services
+{
+    public interface IFileStorage
+    {
+        Task<DirectoryMetadata> CreateDirectory(CreateDirectory command);
+        Task RenameDirectory(RenameDirectory command);
+        Task DeleteDirectory(DeleteDirectory command);
+
+        Task<FileMetadata> CreateFile(CreateFile command);
+        Task AppendFile(AppendFile command);
+        Task DiscardFile(DiscardFile command);
+
+        Task FinalizeFile(FinalizeFile command);
+        Task DeleteFile(DeleteFile command);
+
+
+        Task<DirectoryMetadata[]> GetDirectories(GetDirectory query);
+        Task<FileMetadata[]> ListDirectory(ListDirectory query);
+
+        Task<Stream> GetFile(GetFile query);
+    }
+}
